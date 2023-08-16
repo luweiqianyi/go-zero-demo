ffe5fb0dfbd9c13e993e1c111297126c48e3314c
LuWeiQianYi <runningriven@163.com>
2023-08-16 10:36:18 +0800

    新增api请求，自动生成对应代码

--- cmd/account/internal/handler/routes.go
+++ cmd/account/internal/handler/routes.go
@@ -17,6 +17,11 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 				Path:    "/from/:name",
 				Handler: AccountHandler(serverCtx),
 			},
+			{
+				Method:  http.MethodPost,
+				Path:    "/register",
+				Handler: RegisterHandler(serverCtx),
+			},
 		},
 	)
 }
--- cmd/account/internal/types/types.go
+++ cmd/account/internal/types/types.go
@@ -2,9 +2,24 @@
 package types
 
 type Request struct {
-	Name string `path:"name"`//,options=you|me
+	Name string `path:"name"`
 }
 
 type Response struct {
 	Message string `json:"message"`
+}
+
+type CommonResp struct {
+	Result  string `json:"result"`
+	Message string `json:"message,omitempty"` // omitempty: allow to omit
+}
+
+type RegisterReq struct {
+	AccountName string `json:"accountName"`
+	Password    string `json:"password"`
+}
+
+type RegisterResp struct {
+	CommonResp
+	AccessToken string `json:"token"`
 }
